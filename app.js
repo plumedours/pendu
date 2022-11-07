b888d9306884e33559c5404f30da3d1105aa1388
plumedours <97360012+plumedours@users.noreply.github.com>
2022-11-07 12:44:49 +0100

    Mise à jour du script
    Ajout du score
    Ajout des images en fonction du score

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const newGameBtn = document.getElementById("newGameBtn");
 const lettersBtn = document.querySelectorAll(".letter");
 const findWord = document.getElementById("findWord");
+const wrongLabel = document.getElementById("wrongLabel");
+const penduImg = document.getElementById("penduImg");
 
+let srcImg = 0;
 let inGame = false; //partie en cours ou non
-let wrong = 0; //pour compter le nbr d'erreur
+let wrong = 7; //pour compter le nbr d'erreur
 let arrayWord = []; //tableau pour split le mot en cours
 let words = 0; //pour compter le nombre de mot du tableau
 let wordsPlayed = ""; //pour garder en mémoire les mots déjà joués
@@ -29,18 +32,24 @@ newGameBtn.addEventListener("click", () => {
 });
 
 lettersBtn.forEach((letter) => {
-  letter.addEventListener("click", () => {
-    console.log(letter.textContent);
-    for (let i = 0; i < arrayWord.length; i++) {
-      if (letter.textContent === arrayWord[i]) {
-        hidden_word.splice(arrayWord.indexOf(arrayWord[i]), 1, arrayWord[i]);
-        console.log(arrayWord.indexOf(arrayWord[i]));
-        findWord.textContent = hidden_word.join(" ");
-        console.log(hidden_word);
-        // console.log(true);
-      } else {
-        // console.log(false);
+    letter.addEventListener("click", () => {
+      console.log(letter.textContent);
+      for (let i = 0; i < arrayWord.length; i++) {
+        if (letter.textContent === arrayWord[i]) {
+          hidden_word.splice(i, 1, letter.textContent);
+          console.log(arrayWord.indexOf(arrayWord[i]));
+          findWord.textContent = hidden_word.join(" ");
+          console.log(hidden_word);
+          // console.log(true);
+        } else {
+            // console.log(false);
+        }
       }
-    }
+      if(!wordInGame.includes(letter.textContent)) {
+        srcImg ++;
+        wrong --;
+        wrongLabel.textContent = wrong;
+        penduImg.src="./img/hangman_" + srcImg + ".png";
+      }
+    });
   });
-});
